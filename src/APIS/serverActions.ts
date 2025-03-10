c701349cb31b1d6928cb8f6c94d5582e80e7dabe
Mrdynamic-soni <ssoni5930@gmail.com>
2025-03-10 18:03:31 +0530

    gg

--- src/APIS/serverActions.ts
+++ src/APIS/serverActions.ts
@@ -1,11 +1,11 @@
 "use server";
 
-import { RECAPTCHA_URL } from "@/util/urls";
 import axios from "axios";
 
 export async function verifyCaptcha(token: string | null) {
-  const url = `${RECAPTCHA_URL}${process.env.NEXT_PUBLIC_RECAPTCHA_SECRET_KEY}&response=${token}`;
+  const url = `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.NEXT_PUBLIC_RECAPTCHA_SECRET_KEY}&response=${token}`;
   const res = await axios.post(url);
+  console.log("reCAPTCHA response:", res.data); // Debugging
   if (res.data.success) {
     return "success!";
   } else {
