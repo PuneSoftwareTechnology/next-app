0865b5b1ebe48d0aff3f5bd2efec919f9d15e66a
Mrdynamic-soni <ssoni5930@gmail.com>
2025-03-10 16:19:16 +0530

    fixes

--- src/APIS/serverActions.ts
+++ src/APIS/serverActions.ts
@@ -4,9 +4,7 @@ import { RECAPTCHA_URL } from "@/util/urls";
 import axios from "axios";
 
 export async function verifyCaptcha(token: string | null) {
-  const url = `${RECAPTCHA_URL}${process.env.RECAPTCHA_SECRET_KEY}&response=${token}`;
-  console.log(url);
-
+  const url = `${RECAPTCHA_URL}${process.env.NEXT_PUBLIC_RECAPTCHA_SECRET_KEY}&response=${token}`;
   const res = await axios.post(url);
   if (res.data.success) {
     return "success!";
