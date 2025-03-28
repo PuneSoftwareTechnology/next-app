6163b3cf32436fcda5ef207cbe8559af64bf4053
Mrdynamic-soni <ssoni5930@gmail.com>
2025-03-28 11:26:18 +0530

    updated Url of AWs

--- src/util/urls.ts
+++ src/util/urls.ts
@@ -1,2 +1,2 @@
 export const LOCAL_URL = "http://localhost:8080";
-export const BASE_URL = "https://express-app-pla4.onrender.com";
+export const BASE_URL = "https://zapft46tc4.execute-api.ap-south-1.amazonaws.com";
